32be185639ef3a547c8d23dc1123cdc6ea1d0fa3
M Sathish kumar <klmsathish@gmail.com>
2020-12-13 19:05:04 +0530

    updated

--- FramesAuthMail/settings.py
+++ FramesAuthMail/settings.py
@@ -130,5 +130,5 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'klmsathish.sret@gmail.com'
-EMAIL_HOST_PASSWORD = 'satjun321'
+EMAIL_HOST_USER = 'thiruvengadam.e0119023@sret.edu.in'
+EMAIL_HOST_PASSWORD = 'twxtugqtatxdnsbo'
